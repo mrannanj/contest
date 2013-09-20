c1e86bc7166fa3d3103c4f89e6cd6ddc78f2ef34
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2013-09-20 11:12:24 +0300

    summat

---
+++ w1/summat.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void summa() {
+  int x;
+  cin >> x;
+  int n = 0;
+  for (int a = 0; a <= 32; ++a) {
+    for (int b = 0; b <= 32; ++b) {
+      for (int c = 0; c <= 32; ++c) {
+        for (int d = 0; d <= 32; ++d) {
+          n += (a*a + b*b + c*c + d*d == x);
+        }
+      }
+    }
+  }
+  cout << n << endl;
+}
+
+int main(void) {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    summa();
+  }
+  return 0;
+}
+
